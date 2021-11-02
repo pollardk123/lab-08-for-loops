553fcac9de443aae813b35996ba7ad7e85308355
pollardk123 <pollardk836@gmail.com>
2021-11-01 23:57:31 -0400

    created average.html and average.py

---
+++ average.py
@@ -0,0 +1,11 @@
+average = 0
+sum = 0
+
+for i in range(0, 4,):
+    userinput= input("Number please... ")
+    usernum = int(userinput)
+    sum = sum + usernum
+    print("user input is : " + str(usernum) + " and the current sum is : " + str(sum))
+
+average = sum / 4
+print("The average is : " + str(average))
