4e4779fb644b6deb3ac58c0c0587442ec55f9448
pollardk123 <pollardk836@gmail.com>
2021-11-01 22:15:18 -0400

    created tens html and tens py

---
+++ tens.py
@@ -0,0 +1,9 @@
+# In JS :
+# for (var i = 20; i <= 100; i = i + 10) {
+#   console.log(i);
+# }
+
+for i in range (20, 110, 10):
+    print(i)
+
+# range (<initial value>, <termination condition>, <increment>)
